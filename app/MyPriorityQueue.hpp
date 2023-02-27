2801f7b608e87a17c1e6ef20533d32e9a5e68566
Eric Minassian <ehminass@uci.edu>
2023-02-27 15:37:32 -0800

    initial commit

--- app/MyPriorityQueue.hpp
+++ app/MyPriorityQueue.hpp
@@ -2,97 +2,119 @@
 #define __PROJ5_PRIORITY_QUEUE_HPP
 
 #include "runtimeexcept.hpp"
+#include <vector>
 
-class PriorityQueueEmptyException : public RuntimeException 
-{
+class PriorityQueueEmptyException : public RuntimeException {
 public:
-	PriorityQueueEmptyException(const std::string & err) : RuntimeException(err) {}
+  PriorityQueueEmptyException(const std::string &err) : RuntimeException(err) {}
 };
 
-
-
-
-template<typename Object>
-class MyPriorityQueue
-{
+template <typename Object> class MyPriorityQueue {
 private:
-	// fill in private member data here
-	Object o; // this is only here for use in a stub, you probably want to delete this.
-
+  std::vector<Object> heap;
 
 public:
+  // You also need a constructor and a destructor.
+  MyPriorityQueue();
+  ~MyPriorityQueue();
 
-	// You also need a constructor and a destructor.
-	MyPriorityQueue();
-	~MyPriorityQueue();
+  size_t size() const noexcept;
+  bool isEmpty() const noexcept;
 
- 	size_t size() const noexcept;
-	bool isEmpty() const noexcept;
+  void insert(const Object &elem);
 
-	void insert(const Object & elem);
+  // Note:  no non-const version of this one.  This is on purpose because
+  // the interior contents should not be able to be modified due to the
+  // heap property.  This isn't true of all priority queues but
+  // for this project, we will do this.
+  // min and extractMin should throw PriorityQueueEmptyException if the queue is
+  // empty.
+  const Object &min() const;
 
-
-	// Note:  no non-const version of this one.  This is on purpose because
-	// the interior contents should not be able to be modified due to the
-	// heap property.  This isn't true of all priority queues but 
-	// for this project, we will do this.
-	// min and extractMin should throw PriorityQueueEmptyException if the queue is empty.
-	const Object & min() const; 
-
-	void extractMin(); 
-
+  void extractMin();
 };
 
+template <typename Object>
+MyPriorityQueue<Object>::MyPriorityQueue() : heap({}) {}
 
-template<typename Object>
-MyPriorityQueue<Object>::MyPriorityQueue()
-{
+template <typename Object> MyPriorityQueue<Object>::~MyPriorityQueue() {}
 
+template <typename Object>
+size_t MyPriorityQueue<Object>::size() const noexcept {
+  return (size_t)heap.size();
 }
 
-
-template<typename Object>
-MyPriorityQueue<Object>::~MyPriorityQueue()
-{
-	
+template <typename Object>
+bool MyPriorityQueue<Object>::isEmpty() const noexcept {
+  return heap.empty();
 }
 
-template<typename Object>
-size_t MyPriorityQueue<Object>::size() const noexcept
-{
-	return 500; // stub
+template <typename Object>
+void MyPriorityQueue<Object>::insert(const Object &elem) {
+  heap.push_back(elem);
+
+  int cur = heap.size() - 1;
+  int parent = (int)((cur - 1) / 2);
+
+  while (parent > -1 && heap[cur] < heap[parent]) {
+    std::swap(heap[cur], heap[parent]);
+    cur = parent;
+    parent = (int)((cur - 1) / 2);
+  }
 }
 
+template <typename Object> const Object &MyPriorityQueue<Object>::min() const {
+  if (isEmpty()) {
+    throw PriorityQueueEmptyException("Priority queue is empty");
+  }
 
-
-template<typename Object>
-bool MyPriorityQueue<Object>::isEmpty() const noexcept
-{
-	return true; // stub
+  return heap[0];
 }
 
-template<typename Object>
-void MyPriorityQueue<Object>::insert(const Object & elem) 
-{
-	// stub
+template <typename Object> void MyPriorityQueue<Object>::extractMin() {
+  if (isEmpty()) {
+    throw PriorityQueueEmptyException("Priority queue is empty");
+  } else if (heap.size() == 1) {
+    heap.pop_back();
+  } else {
+    std::swap(heap[0], heap[heap.size() - 1]);
+    int cur = 0;
+    int child_1 = 2 * cur + 1, child_2 = 2 * cur + 2;
+
+    heap.pop_back();
+
+    while (child_1 < heap.size() || child_2 < heap.size()) {
+      if (child_1 < heap.size() && child_2 < heap.size()) {
+        int min_index = heap[child_1] < heap[child_2] ? child_1 : child_2;
+        if (heap[cur] > heap[min_index]) {
+          std::swap(heap[cur], heap[min_index]);
+          cur = min_index;
+          child_1 = 2 * cur + 1;
+          child_2 = 2 * cur + 2;
+        } else {
+          break;
+        }
+      } else if (child_1 < heap.size()) {
+        if (heap[cur] > heap[child_1]) {
+          std::swap(heap[cur], heap[child_1]);
+          cur = child_1;
+          child_1 = 2 * cur + 1;
+          child_2 = 2 * cur + 2;
+        } else {
+          break;
+        }
+      } else {
+        if (heap[cur] > heap[child_2]) {
+          std::swap(heap[cur], heap[child_2]);
+          cur = child_2;
+          child_1 = 2 * cur + 1;
+          child_2 = 2 * cur + 2;
+        } else {
+          break;
+        }
+      }
+    }
+  }
 }
 
-
-
-
-template<typename Object>
-const Object & MyPriorityQueue<Object>::min() const
-{
-	return o; // terrible idea, don't actually do this.
-}
-
-
-
-template<typename Object>
-void MyPriorityQueue<Object>::extractMin() 
-{
-	// stub
-}
-
-
-#endif 
+#endif
--- app/convert.cpp
+++ app/convert.cpp
@@ -1,35 +1,93 @@
 #include "convert.hpp"
-#include <string>
+#include "MyPriorityQueue.hpp"
+#include <map>
 #include <sstream>
-#include <unordered_set>
 #include <string>
+#include <unordered_set>
+#include <vector>
 
 // You should not need to change this function.
-void loadWordsIntoTable(std::unordered_set<std::string> & words, std::istream & in)
-{
-	std::string line, word;
-	std::stringstream ss;
+void loadWordsIntoTable(std::unordered_set<std::string> &words,
+                        std::istream &in) {
+  std::string line, word;
+  std::stringstream ss;
 
-	while(	getline(in, line) )
-	{
-		ss.clear();
-		ss << line;
-		while( ss >> word )
-		{
-			words.insert( word );
-		}
-	}
-
+  while (getline(in, line)) {
+    ss.clear();
+    ss << line;
+    while (ss >> word) {
+      words.insert(word);
+    }
+  }
 }
 
+struct Node {
+  std::string word;
+  unsigned value;
 
+  Node(const std::string &word, unsigned value) : word(word), value(value) {}
 
-// You probably want to change this function.
-std::vector< std::string > convert(const std::string & s1, const std::string & s2, const std::unordered_set<std::string> & words)
-{
-	std::vector< std::string > ret;
-	ret.push_back("foo");
-	ret.push_back("bar");
-	return ret;  // stub obviously 
+  bool operator<(const Node &other) const { return value < other.value; }
+  bool operator>(const Node &other) const { return value > other.value; }
+};
+
+std::vector<std::string> convert(const std::string &s1, const std::string &s2,
+                                 const std::unordered_set<std::string> &words) {
+
+  if (s1.size() != s2.size()) {
+    return {};
+  }
+
+  if (s1 == "" || s2 == "") {
+    return {};
+  }
+
+  std::vector<std::string> result;
+  std::map<std::string, std::string> map;
+  MyPriorityQueue<Node> queue;
+
+  Node node(s1, 0);
+  queue.insert(node);
+  map[s1] = "";
+
+  while (!queue.isEmpty()) {
+    Node node = queue.min();
+    queue.extractMin();
+
+    if (node.word == s2) {
+      std::string temp = node.word;
+      while (temp != "") {
+        result.push_back(temp);
+        temp = map[temp];
+      }
+      for (unsigned i = 0; i < result.size() / 2; i++) {
+        std::swap(result[i], result[result.size() - i - 1]);
+      }
+      return result;
+    }
+
+    for (unsigned i = 0; i < node.word.size(); i++) {
+      for (char c = 'a'; c <= 'z'; c++) {
+        std::string temp = node.word;
+        temp[i] = c;
+
+        if (words.find(temp) != words.end() && map.find(temp) == map.end()) {
+          map[temp] = node.word;
+
+          unsigned lc_distance = node.value + 1, num_diff = 0;
+          for (unsigned j = 0; j < temp.size(); j++) {
+            if (temp[j] != s2[j]) {
+              num_diff++;
+            }
+          }
+
+          Node newNode(temp, lc_distance + num_diff);
+
+          queue.insert(newNode);
+          map[temp] = node.word;
+        }
+      }
+    }
+  }
+  return {};
 }
-
